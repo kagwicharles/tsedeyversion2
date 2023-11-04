7fb5984bcf02e286c4f467da7e15722d66f8094a
kagwicharles <kagwitheuri@gmail.com>
2023-11-04 22:15:56 +0300

    update: ui/ux changes

--- lib/src/ui/auth/login_screen.dart
+++ lib/src/ui/auth/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:craft_dynamic/craft_dynamic.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_launcher_icons/config/config.dart';
+import 'package:get/get.dart';
 import 'package:pinput/pinput.dart';
 import 'package:tsedeybnk/src/theme/app_theme.dart';
 
@@ -46,147 +47,154 @@ class _LoginScreenState extends State<LoginScreen> {
                       fit: BoxFit.cover)),
               child: Column(
                 children: [
-                  const Expanded(flex: 5, child: SizedBox()),
                   Expanded(
-                      flex: 5,
-                      child: Stack(
-                        children: [
-                          Card(
-                              margin: EdgeInsets.zero,
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(40),
-                                      topRight: Radius.circular(40))),
-                              child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 14,
-                                    horizontal: 20,
-                                  ),
-                                  child: widget.isLoadingScreen
-                                      ? CircularLoadUtil(
-                                          colors: [
-                                            AppTheme.primaryColor,
-                                            AppTheme.secondaryAccent
-                                          ],
-                                        )
-                                      : Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.stretch,
-                                          mainAxisSize: MainAxisSize.max,
+                    flex: 5,
+                    child: Container(color: Colors.transparent),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Card(
+                        margin: EdgeInsets.zero,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(40),
+                                topRight: Radius.circular(40))),
+                        child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                            ),
+                            child: widget.isLoadingScreen
+                                ? Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                          "assets/img/login_eclipse.png",
+                                          height: 150,
+                                          width: 150,
+                                          fit: BoxFit.contain),
+                                      const SizedBox(
+                                        height: 12,
+                                      ),
+                                      CircularLoadUtil(
+                                        colors: [
+                                          AppTheme.primaryColor,
+                                          AppTheme.secondaryAccent
+                                        ],
+                                      )
+                                    ],
+                                  )
+                                : SingleChildScrollView(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.stretch,
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Image.asset(
+                                            "assets/img/login_eclipse.png",
+                                            height: 150,
+                                            width: 150,
+                                            fit: BoxFit.contain),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        const Text(
+                                          "Enter your Login PIN",
+                                          style: TextStyle(
+                                              color: Colors.grey, fontSize: 18),
+                                        ),
+                                        const SizedBox(
+                                          height: 12,
+                                        ),
+                                        Pinput(
+                                          obscuringCharacter: "*",
+                                          enabled: widget.isLoadingScreen
+                                              ? false
+                                              : true,
+                                          length: 4,
+                                          controller: _pinController,
+                                          autofocus: widget.isLoadingScreen
+                                              ? false
+                                              : true,
+                                          obscureText: true,
+                                          defaultPinTheme: PinTheme(
+                                              height: 60,
+                                              width: 60,
+                                              textStyle: TextStyle(
+                                                  color: APIService
+                                                      .appPrimaryColor,
+                                                  fontSize: 24,
+                                                  fontWeight: FontWeight.bold),
+                                              padding: const EdgeInsets.all(4),
+                                              decoration: BoxDecoration(
+                                                  border: Border.all(
+                                                      color: AppTheme
+                                                          .primaryColor),
+                                                  borderRadius:
+                                                      const BorderRadius.all(
+                                                          Radius.circular(
+                                                              12)))),
+                                          forceErrorState: true,
+                                          pinputAutovalidateMode:
+                                              PinputAutovalidateMode.onSubmit,
+                                          onCompleted: (pin) {
+                                            _login(pin);
+                                          },
+                                        ),
+                                        const SizedBox(
+                                          height: 24,
+                                        ),
+                                        Row(
                                           children: [
+                                            Expanded(
+                                                child: ElevatedButton(
+                                                    onPressed: () {},
+                                                    child:
+                                                        const Text("Login"))),
                                             const SizedBox(
-                                              height: 12,
+                                              width: 16,
                                             ),
-                                            const Text(
-                                              "Enter your Login PIN",
-                                              style: TextStyle(
-                                                  color: Colors.grey,
-                                                  fontSize: 18),
-                                            ),
-                                            const SizedBox(
-                                              height: 12,
-                                            ),
-                                            Pinput(
-                                              obscuringCharacter: "*",
-                                              enabled: widget.isLoadingScreen
-                                                  ? false
-                                                  : true,
-                                              length: 4,
-                                              controller: _pinController,
-                                              autofocus: widget.isLoadingScreen
-                                                  ? false
-                                                  : true,
-                                              obscureText: true,
-                                              defaultPinTheme: PinTheme(
-                                                  height: 60,
-                                                  width: 60,
-                                                  textStyle: TextStyle(
-                                                      color: APIService
-                                                          .appPrimaryColor,
-                                                      fontSize: 24,
-                                                      fontWeight:
-                                                          FontWeight.bold),
+                                            InkWell(
+                                                borderRadius:
+                                                    BorderRadius.circular(12),
+                                                onTap: () {
+                                                  _authRepo.biometricLogin(
+                                                      _pinController,
+                                                      isButtonAction: true);
+                                                },
+                                                child: Container(
                                                   padding:
-                                                      const EdgeInsets.all(4),
+                                                      const EdgeInsets.all(12),
                                                   decoration: BoxDecoration(
-                                                      border: Border.all(
-                                                          color: AppTheme
-                                                              .primaryColor),
                                                       borderRadius:
                                                           const BorderRadius
                                                               .all(
                                                               Radius.circular(
-                                                                  12)))),
-                                              forceErrorState: true,
-                                              pinputAutovalidateMode:
-                                                  PinputAutovalidateMode
-                                                      .onSubmit,
-                                              onCompleted: (pin) {
-                                                _login(pin);
-                                              },
-                                            ),
-                                            SizedBox(
-                                              height: 24,
-                                            ),
-                                            Row(
-                                              children: [
-                                                Expanded(
-                                                    child: ElevatedButton(
-                                                        onPressed: () {},
-                                                        child: const Text(
-                                                            "Login"))),
-                                                const SizedBox(
-                                                  width: 16,
-                                                ),
-                                                InkWell(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            12),
-                                                    onTap: () {
-                                                      _authRepo.biometricLogin(
-                                                          _pinController,
-                                                          isButtonAction: true);
-                                                    },
-                                                    child: Container(
-                                                      padding:
-                                                          EdgeInsets.all(12),
-                                                      decoration: BoxDecoration(
-                                                          borderRadius:
-                                                              BorderRadius.all(
-                                                                  Radius
-                                                                      .circular(
-                                                                          12)),
-                                                          border: Border.all(
-                                                              color: APIService
-                                                                  .appPrimaryColor)),
-                                                      child: Icon(
-                                                        Icons.fingerprint,
-                                                        color: Colors.black,
-                                                        size: 28,
-                                                      ),
-                                                    )),
-                                              ],
-                                            ),
-                                            const SizedBox(
-                                              height: 44,
-                                            ),
-                                            if (_isLoading)
-                                              AppLoader(
-                                                useWhite: false,
-                                              ),
-                                            const SizedBox(
-                                              height: 12,
-                                            )
+                                                                  12)),
+                                                      border: Border.all(
+                                                          color: APIService
+                                                              .appPrimaryColor)),
+                                                  child: const Icon(
+                                                    Icons.fingerprint,
+                                                    color: Colors.black,
+                                                    size: 28,
+                                                  ),
+                                                )),
                                           ],
-                                        ))),
-                          Positioned(
-                              left: 0,
-                              right: 0,
-                              top: -300,
-                              child:
-                                  Image.asset("assets/img/login_eclipse.png")),
-                        ],
-                      ))
+                                        ),
+                                        const SizedBox(
+                                          height: 44,
+                                        ),
+                                        if (_isLoading)
+                                          AppLoader(
+                                            useWhite: false,
+                                          ),
+                                        const SizedBox(
+                                          height: 12,
+                                        )
+                                      ],
+                                    ),
+                                  ))),
+                  )
                 ],
               )),
           backgroundColor: Theme.of(context).primaryColor,
